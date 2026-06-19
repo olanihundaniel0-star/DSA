c38f45d9dc34a622481baaae59c6f77ba95a63ab
olanihundaniel0-star <olanihundaniel0@gmail.com>
2026-06-19 22:31:16 +0100

    solution to reversed linked list problem

---
+++ reversed-linked-list.py
@@ -0,0 +1,14 @@
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None  # sets previous to None
+        curr = head # the current node is basically your head, im starting from the first index(node)
+
+        while curr: # while curr is true
+            nxt = curr.next # saves your next pointer before resetting
+            curr.next = prev # sets your next pointer to previous
+            prev = curr # then previous becomes the old current 
+            curr = nxt # then your new current becomes next
+
+        return prev 
